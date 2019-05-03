d09f925d2ae11d567ab38ab171651ac745fbbd0e
Omarmok98 <omarmok@icloud.com>
2019-05-03 14:34:41 +0200

    GetPosts

--- server.js
+++ server.js
@@ -20,6 +20,33 @@ db.connect((err)=>{
 const app = express();
 app.listen(Port,()=>console.log(`listening on port ${Port}...`));
 
+class user
+{
+    static showPosts(userId,callback)
+    {
+        let sql = `SELECT * FROM community.post WHERE post.USERID IN 
+        (SELECT distinct FOLLOWED_USERID
+        FROM community.followuser,community.followcompany,community.normaluser
+        WHERE community.followuser.FOLLOW_USERID = '${userId}' and community.followcompany.USERID='${userId}')
+        
+        union
+        
+        SELECT * FROM community.post WHERE post.COMPANYID IN
+        (SELECT distinct companyID
+        FROM community.followuser,community.followcompany,community.normaluser
+        WHERE community.followuser.FOLLOW_USERID = '${userId}' and community.followcompany.USERID='${userId}')`;
+
+        db.query(sql,function(err,result){
+            if (err) callback(err,null);
+            else callback(null,result); 
+        });
+    }
+
+   
+        
+    
+    
+}
 class post
 {
     static addPostUser(userId, content)
@@ -57,13 +84,6 @@ class post
             console.log(result)
         });
         return Post;
-    }
-    static showposts(userId)
-    {
-        let sql = `SELECT distinct FOLLOWED_USERID,companyID
-        FROM community.followuser,community.followcompany,community.normaluser
-        WHERE community.followuser.FOLLOW_USERID = ${userId} and community.followcompany.USERID=${userId};`
-        
     }
     
 }
@@ -129,6 +149,17 @@ app.post('/addPostUser/:userId',(req,res)=>{
 });
 app.post('/addPostCompany/:companyId',(req,res)=>{
     res.send(post.addPostCompany(req.params.companyId,"wanna a scholarship"));
+});
+app.get('/getposts/:userId',(req,res)=>{
+     
+    user.showPosts(req.params.userId,function(err,data){
+        if (err) {
+            // error handling code goes here
+            console.log("ERROR : ",err);             
+        } else {            
+            res.end(JSON.stringify(data));   
+        }    
+    });
 });
 
 app.get('/',(req, res) =>{
